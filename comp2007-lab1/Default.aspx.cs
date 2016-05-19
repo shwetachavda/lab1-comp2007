62239f425d29a3863560121d20e9487267628f8e
shwetachavda <shwetachavda13@gmail.com>
2016-05-19 14:26:35 -0400

    initial commit

---
+++ comp2007-lab1/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace comp2007_lab1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SubmitButton_Click(object sender, EventArgs e)
+        {
+            
+        
+            FullnameTextBox.Text = FirstNameTextBox.Text.ToString() + " " + LastNameTextBox.Text.ToString();
+        
+    }
+    }
+}
